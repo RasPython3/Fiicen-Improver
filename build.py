226b9f97dfc58b389955ec675934a64c777bc440
RasPython3 <87003300+RasPython3@users.noreply.github.com>
2025-03-11 12:34:46 +0900

    Generate zip files

--- build.py
+++ build.py
@@ -1,10 +1,17 @@
 import os
+import glob
 import shutil
 import zipfile
 import json
 
-def zip_files(files, output, prefix=""):
-    pass
+def zip_files(directory, output):
+    files = glob.glob("**", root_dir=directory, recursive=True)
+    dirs = glob.glob("**/", root_dir=directory, recursive=True)
+    with zipfile.ZipFile(output, "w", compression=zipfile.ZIP_DEFLATED) as zf:
+        for file in files:
+            if file not in dirs and file + os.sep not in dirs:
+                zf.write(f"{directory}/{file}", arcname=file.replace(os.sep, "/"))
+        
 
 def build_chrome():
     shutil.copy("LICENSE", "build/chrome/Fiicen-Improver/LICENSE")
@@ -35,13 +42,18 @@ def build():
     except:
         pass
 
+    with open("src/manifest.json", mode="r") as f:
+        version = json.load(f)["version"]
+
     os.makedirs("build/chrome")
 
     shutil.copytree("src", "build/chrome/Fiicen-Improver")
     build_chrome()
+    zip_files("build/chrome", f"build/chrome-{version}.zip")
 
     shutil.copytree("src", "build/firefox")
     build_firefox()
+    zip_files("build/firefox", f"build/firefox-{version}.zip")
 
 if __name__ == "__main__":
     build()
